435692c6d2d25025095bc7b8c845ece5a2f44de8
IntuMedix <intumedix@gmail.com>
2026-07-19 00:54:44 +0300

    Fix PWA caching file size limit

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,7 @@ export default defineConfig({
         screenshots: [],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
